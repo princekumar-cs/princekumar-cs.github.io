f7cde9b74d8cb7b687dbeec1ea28f5a48ec3dc8f
Prince Kumar <princekumar.csit@gmail.com>
2025-10-05 10:49:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -46,8 +46,8 @@ themeBtn.addEventListener("click", () => {
   themeBtn.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
 });
 // Education Card Popup
-const eduCard = document.querySelector("#btechcard"); // First card (B.Tech)
-const modal = document.getElementById("#gradeModal");
+const eduCard = document.querySelector(".btechcard"); // First card (B.Tech)
+const modal = document.getElementById("gradeModal");
 const closeBtn = document.getElementById("closeModal");
 
 // Open modal
@@ -66,6 +66,7 @@ modal.addEventListener("click", (e) => {
     modal.style.display = "none";
   }
 });
+
 
 
 
